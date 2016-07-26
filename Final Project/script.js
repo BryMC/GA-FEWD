e56addae66398a61904b232feb5ce52dc08c1cc1
BryMC <bryonycmclaughlin@Bryonys-MacBook-Pro.local>
2016-07-25 22:38:00 -0500

    final project rough draft

---
+++ Final Project/script.js
@@ -0,0 +1,50 @@
+$(document).ready(function() {
+	var catalogue = ['anthropology', 'architecture', 'art', 'art history', 'biochemistry', 'biology', 'business','chemistry', 'chinese', 'classics', 'communications', 'computer science', 'economics', 'education', 'english', 'environmental studies', 'feminist studies', 'french', 'german', 'greek','history','international studies', 'kinesiology','latin','mathematics','music','philosophy','physics','political science','psychology','religion','sociology','spanish','theatre'];
+	catalogue[15];
+}
+});
+	//$('#button').click('begin');
+	//user clicks one input
+	//or 
+	//user clicks two inputs
+	//if user clicks one input
+	//loop through this array and find the appropriate tag
+	//once you find it
+	//output all information from that depts array
+	//in the order it appears (loop)
+	//or if they click two inputs
+	//loop through input 1
+	//and loop through input 2
+	//and find connections
+	//and output only those related to both
+	//if user clicks on a department
+	//or on a class
+	//link to a separate screen that has info about that connection
+	
+//array databases
+// psychology = {'principles of psychology', 'research methods', 'human sexuality', 'social psychology', 'cognitive psychology', 'animal behavior', 'cognitive neuroscience', 'health psychology', 'abnormal psychology'}
+// theater || theatre = {'fundamentals of acting', 'movement techniques', 'theater history', 'east meets west', 'costume design', 'set design', 'principles of design', 'sustainable theater'}
+//business = {'foundations of business', 'principles of economics', 'introduction to accounting', consumer behavior', 'emarketing', 'foundations of entrepreneurship'}
+//music = {'music theory I', 'music theory II', 'solfrege', 'music theory', 'world music'}
+// var catalogue = {'anthropology', 'architecture', 'art', 'art history', 'biochemistry', 'biology', 'business','chemistry', 'chinese', 'classics', 'communications', 'computer science', 'economics', 'education', 'english', 'environmental studies', 'feminist studies', 'french', 'german', 'greek','history','international studies', 'kinesiology','latin','mathematics','music','philosophy','physics','political science','psychology','religion','sociology','spanish','theatre'}
+
+//if begin button is clicked call start function
+//start function () {}
+//console.log("Do you want to search one discipline or connect two disciplines?");
+//if one discipline button is clicked call 1Disc function
+//else if two discipline button is clicked called 2Disc function
+//if 1Disc function called, wait for user input of name (must enter with #)
+	//if find but is clicked call searchCat function
+			//loop through catalogue array to find correct array match with user string
+			//once found, output contents of that array on console log as a list
+			//end function
+//if 2Disc function called, wait for user to input both names (must enter two)
+	//if find button is clicked call searchCat function
+	//loop through catalogue array to find first input match, once found store contents in temp array
+	//loop through catalogue array again to find second input match, once found store contents in temp array2
+	//if both inputs match contents of catalogue and are stored in temp array return true
+	//output contents of both arrays
+	//end function
+//if Connect! button is pressed (after 2Disc completed)
+//loop through contents of array1 and match to contents of array2
+//end function
